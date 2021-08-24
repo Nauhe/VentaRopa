7d16323a03429723cd310a39f62031aff76afa30
Yaniroman <yanina.roman@reserv.com.ar>
2021-08-24 16:52:54 -0300

    creacion componente ropa

---
+++ ventaRopa/src/app/ropa/ropa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ropa',
+  templateUrl: './ropa.component.html',
+  styleUrls: ['./ropa.component.scss']
+})
+export class RopaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
