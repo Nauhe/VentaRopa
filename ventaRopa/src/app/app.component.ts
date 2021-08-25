95650bb0b3b55c4adfe474661173975b939a9abf
Yaniroman <yanina.roman@reserv.com.ar>
2021-08-25 13:20:02 -0300

    input en ropa

--- ventaRopa/src/app/app.component.ts
+++ ventaRopa/src/app/app.component.ts
@@ -7,5 +7,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ventaRopa';
+  tituloMujer= "Mujer"; 
+
   
+  productoMujer: any[] = [
+    {
+    descripcion: 'Remera',
+    precio: 2000
+  },
+  {
+    descripcion: 'Buzo',
+    precio: 8000
+  },
+  {
+    descripcion: 'Pantalon',
+    precio: 5000
+  },
+  ]
+
+  productoHombre: any[] = [
+    {
+    descripcion: 'Remera',
+    precio: 2000
+  },
+  {
+    descripcion: 'Campera',
+    precio: 9000
+  },
+  {
+    descripcion: 'Pantalon',
+    precio: 8000
+  },
+  ]
+
+  productoNinio: any[] = [
+    {
+    descripcion: 'Remera',
+    precio: 2000
+  },
+  {
+    descripcion: 'Buzo',
+    precio: 8000
+  },
+  {
+    descripcion: 'Pantalon',
+    precio: 5000
+  }
+  ]
+
+
 }
