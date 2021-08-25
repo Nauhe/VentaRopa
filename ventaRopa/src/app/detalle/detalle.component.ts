77338e1e416befc9a90b42a17e58e0b3fd842d64
Nauhe <nahuel.valdez@reserv.com.ar>
2021-08-25 16:19:20 -0300

    se act app component y se genero component detalle

---
+++ ventaRopa/src/app/detalle/detalle.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle',
+  templateUrl: './detalle.component.html',
+  styleUrls: ['./detalle.component.scss']
+})
+export class DetalleComponent implements OnInit {
+  @Input() producto: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
