195d7fe49399bfc93b9827ea7a1080d70f1055a3
Nauhe <nahuel.valdez@reserv.com.ar>
2021-08-30 16:39:07 -0300

    funcion de colores

--- ventaRopa/src/app/ropa/ropa.component.ts
+++ ventaRopa/src/app/ropa/ropa.component.ts
@@ -21,7 +21,13 @@ export class RopaComponent implements OnInit {
     this.router.navigate(['detalle']);
   }
 
- 
+  ponerColor(color, producto){
+    if(color =="blanco"){
+      producto.foto= "assets/Remera_mujerBlanca.jpg"
+    }else{
+      producto.foto="assets/remera_mujerNegra.jpg"
+    }
+  }
 
 
 }
