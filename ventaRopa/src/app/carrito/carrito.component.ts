4b5c2314224711e86f09b6bb837ff78a8fd8f6bf
Nauhe <nahuel.valdez@reserv.com.ar>
2021-09-06 10:05:00 -0300

    funciones de carrito

--- ventaRopa/src/app/carrito/carrito.component.ts
+++ ventaRopa/src/app/carrito/carrito.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CarritoService } from '../carrito.service';
 import * as _ from 'lodash';
+import { ProductosService } from '../productos.service';
 
 @Component({
   selector: 'app-carrito',
@@ -12,15 +13,21 @@ export class CarritoComponent implements OnInit {
   productoCarrito: any[]=new Array();
 
 
-  constructor(private carritoService: CarritoService) { }
+  constructor(private carritoService: CarritoService, private productosService:ProductosService) { }
 
   ngOnInit(): void {
     this.productoCarrito = this.carritoService.dameProd();
   }
+  contarProd(){
 
+  }
 
 
   //hacer funcion para contar productos.
+
+  quitarProd(){
+    
+  }
   //hacer funcion para agregar quitar.
 
 }
