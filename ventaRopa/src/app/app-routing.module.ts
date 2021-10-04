79975c5ce93ebf35581d0df1a26baf2294cbf25f
Yaniroman <yanina.roman@reserv.com.ar>
2021-10-04 10:40:11 -0300

    roteo y de alta

--- ventaRopa/src/app/app-routing.module.ts
+++ ventaRopa/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AltaProdComponent } from './alta-prod/alta-prod.component';
 import { AppComponent } from './app.component';
 import { CarritoComponent } from './carrito/carrito.component';
 import { ContenidoComponent } from './contenido/contenido.component';
@@ -7,18 +8,22 @@ import { DetalleComponent } from './detalle/detalle.component';
 
 const routes: Routes = [
   {
-    path:'',
-    component:ContenidoComponent
+    path: '',
+    component: ContenidoComponent
   },
   {
-    path:'detalle',
-    component:DetalleComponent
+    path: 'detalle',
+    component: DetalleComponent
   },
   {
-    path:'carrito',
-    component:CarritoComponent
+    path: 'carrito',
+    component: CarritoComponent
+  },
+  {
+    path: 'alta-prod',
+    component: AltaProdComponent
   }
-  
+
 ];
 
 
